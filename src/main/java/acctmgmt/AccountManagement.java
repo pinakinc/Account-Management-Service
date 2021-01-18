9cb29ed22ca9387bc271fcdc0f129a28b04c223c
pinakin.chaubal <pinakin.chaubal@N10067.intellectdesign.com>
2021-01-18 19:23:23 +0530

    Done

---
+++ src/main/java/acctmgmt/AccountManagement.java
@@ -0,0 +1,18 @@
+package acctmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AccountManagement {
+
+		public static void main(String[] args) {
+			SpringApplication.run(AccountManagement.class, args);
+		}
+	}
+
+
---
+++ src/main/java/acctmgmt/controllers/acctmgmtcntrl.java
@@ -0,0 +1,20 @@
+package acctmgmt.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/acctmgmt")
+
+public class acctmgmtcntrl {
+	
+		
+		@GetMapping("/status/check")
+		public String status() {
+			return "working";
+		}
+
+	}
+
+
